b5cc90f292e650d15566b6e9abcae89765a1ab2f
TechCare <you@example.com>
2022-01-07 19:36:45 +0700

    baitap

--- module2/src/ss5_inheritance/bai_tap/Cylinder.java
+++ module2/src/ss5_inheritance/bai_tap/Cylinder.java
@@ -1,5 +1,29 @@
 package ss5_inheritance.bai_tap;
 
-public class Cylinder {
+public class Cylinder extends Circle {
+    double length;
 
+    public double getLength() {
+        return length;
+    }
+
+    public void setLength(double length) {
+        this.length = length;
+    }
+
+    public Cylinder(double radius, double length, String color) {
+        this.radius = radius;
+        this.length = length;
+        this.color = color;
+    }
+
+    public double getVolume() {
+        return radius * radius * length * Math.PI;
+    }
+
+    @Override
+    public String toString() {
+        return "Cylinder{" + " radius= " + radius + " length= " + length +" color "+ color+'}'
+                + " Volume: " + getVolume();
+    }
 }
---
+++ module2/src/ss5_inheritance/bai_tap/TestCylinder.java
@@ -0,0 +1,8 @@
+package ss5_inheritance.bai_tap;
+
+public class TestCylinder {
+    public static void main(String[] args) {
+        Cylinder cylinder = new Cylinder(2.5,7.5,"blue");
+        System.out.println(cylinder);
+    }
+}
