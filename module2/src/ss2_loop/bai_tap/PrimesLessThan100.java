a5f172559a4983afbc174bb38e68b9e58ad6ee45
TechCare <you@example.com>
2022-01-08 13:40:03 +0700

    baitap

--- module2/src/ss2_loop/bai_tap/PrimesLessThan100.java
+++ module2/src/ss2_loop/bai_tap/PrimesLessThan100.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 
 public class PrimesLessThan100 {
     public static void main(String[] args) {
+        System.out.println("prime <100 is:");
         int count = 0;
         int i = 2;
         while (count < 25) {
@@ -25,4 +26,3 @@ public class PrimesLessThan100 {
         return true;
     }
 }
-
