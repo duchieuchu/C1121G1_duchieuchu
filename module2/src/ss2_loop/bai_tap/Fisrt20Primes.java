6cc5c87eee343e93f87f1545f37ef6ffb6dc438d
TechCare <you@example.com>
2022-01-08 13:25:25 +0700

    baitap

--- module2/src/ss2_loop/bai_tap/Fisrt20Primes.java
+++ module2/src/ss2_loop/bai_tap/Fisrt20Primes.java
@@ -2,11 +2,16 @@ package ss2_loop.bai_tap;
 
 import java.util.Scanner;
 
-public class PrimesLessThan100 {
+public class Fisrt20Primes {
+
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Input numbers of Prime you want: ");
+        int n = scanner.nextInt();//nhap so luong SNT in ra
         int count = 0;
+
         int i = 2;
-        while (count < 25) {
+        while (count < n) {
             if (isPrime(i)) {
                 System.out.print(i + " ");
                 count++;
@@ -25,4 +30,5 @@ public class PrimesLessThan100 {
         return true;
     }
 }
+
 
---
+++ module2/src/ss5_inheritance/bai_tap/Point.java
@@ -0,0 +1,9 @@
+package ss5_inheritance.bai_tap;
+
+public class Point {
+    float x = 0.0f;
+    float y = 0.0f;
+    public Point(){}
+    public Point(float x,float y){
+    }
+}
