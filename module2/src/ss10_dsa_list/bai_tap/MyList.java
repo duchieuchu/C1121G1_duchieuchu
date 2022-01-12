71723fbf2a5a65e33ae7739793f27d97b5e7e02e
TechCare <you@example.com>
2022-01-13 01:02:17 +0700

    baitap

--- module2/src/ss10_dsa_list/bai_tap/MyList.java
+++ module2/src/ss10_dsa_list/bai_tap/MyList.java
@@ -1,4 +1,105 @@
 package ss10_dsa_list.bai_tap;
 
-public class MyList {
+import java.util.Arrays;
+
+public class MyList<E> {
+    private int size = 0;
+    private static final int DEFAULT_CAPACITY = 10;
+    Object elements[];
+    private MyList<E> newList;
+
+    public MyList() {
+        elements = new Object[DEFAULT_CAPACITY];
+    }
+
+    public MyList(int capacity) {
+        elements = new Object[capacity];
+    }
+
+
+    public void add(int index, E element) {
+        if (index > size) {
+            ensureCapacity(size);
+        }
+        this.elements[size++] = elements;
+
+    }
+
+    public E remove(int index) {
+        for (int i = index; i < DEFAULT_CAPACITY; i++) {
+            if (i < size) {
+                elements[i] = elements[i + 1];
+            } else {
+                elements[i] = null;
+            }
+        }
+        size--;
+        return (E) this.elements;
+    }
+
+    public int size() {
+        return this.size;
+    }
+
+    public MyList<E> clone() {
+        for (int i = 0; i < this.size; i++) {
+            newList.add((E) this.elements[i]);
+        }
+        return newList;
+    }
+
+    public boolean contains(E object) {
+        boolean check = true;
+        for (int i = 0; i <= size; i++) {
+            if (elements[i].equals(object)) {
+                check = true;
+                break;
+            } else {
+                check = false;
+            }
+        }
+        if (check) {
+            return true;
+        }
+        return false;
+    }
+
+    public int indexOf(E object) {
+        int index = 0;
+        for (int i = 0; i <= size; i++) {
+            if (elements[i].equals(object)) {
+                index = i;
+                break;
+            }
+        }
+        if (index != 0) {
+            return index;
+        }
+        return -1;
+    }
+
+    public boolean add(E elements) {
+        this.elements[size++] = elements;
+        return true;
+    }
+
+    public void ensureCapacity(int minCapacity) {
+        int newSize = elements.length * 2;
+        elements = Arrays.copyOf(elements, newSize);
+    }
+
+    public E get(int i) {
+        if (i >= size || i < 0) {
+            throw new IndexOutOfBoundsException("Index: " + i + ", Size: " + i);
+        }
+        return (E) elements[i];
+    }
+
+    public void clear() {
+        for (int i = 0; i < size; i++) {
+            elements[i] = null;
+            size = 0;
+        }
+    }
+
 }
--- module2/src/ss10_dsa_list/bai_tap/MyListTest.java
+++ module2/src/ss10_dsa_list/bai_tap/MyListTest.java
@@ -1,4 +1,19 @@
 package ss10_dsa_list.bai_tap;
 
 public class MyListTest {
+    public static void main(String[] args) {
+        MyList<Integer> elementsArray = new MyList<Integer>();
+        elementsArray.add(0, 1);
+        elementsArray.add(1, 2);
+        elementsArray.add(2, 3);
+        elementsArray.add(3, 4);
+        elementsArray.add(4, 5);
+        System.out.println("current size: " + elementsArray.size());
+        System.out.println(elementsArray.get(0));
+        System.out.println(elementsArray.get(1));
+        System.out.println(elementsArray.get(2));
+        System.out.println(elementsArray.get(3));
+        System.out.println(elementsArray.get(4));
+
+    }
 }
