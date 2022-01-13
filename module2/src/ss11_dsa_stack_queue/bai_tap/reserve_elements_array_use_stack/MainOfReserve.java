57e52a6229f6c2e0a524fb6c044b6be7d94ebcc2
TechCare <you@example.com>
2022-01-13 20:39:28 +0700

    baitap

--- module2/src/ss11_dsa_stack_queue/bai_tap/reserve_elements_array_use_stack/MainOfReserve.java
+++ module2/src/ss11_dsa_stack_queue/bai_tap/reserve_elements_array_use_stack/MainOfReserve.java
@@ -1,6 +1,17 @@
 package ss11_dsa_stack_queue.bai_tap.reserve_elements_array_use_stack;
 
 import java.util.Arrays;
+
+import static ss11_dsa_stack_queue.bai_tap.reserve_elements_array_use_stack.Reserve.reverseInterger;
+import static ss11_dsa_stack_queue.bai_tap.reserve_elements_array_use_stack.Reserve.reverseString;
+
 public class MainOfReserve {
+    public static void main(String[] args) {
+        int[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+        reverseInterger(array);
+        System.out.println(Arrays.toString(array));
+        String string = "a,b,c,d,e,f,g,h";
+        System.out.println(reverseString(string));
+    }
 
 }
--- module2/src/ss11_dsa_stack_queue/bai_tap/reserve_elements_array_use_stack/Reserve.java
+++ module2/src/ss11_dsa_stack_queue/bai_tap/reserve_elements_array_use_stack/Reserve.java
@@ -1,7 +1,28 @@
 package ss11_dsa_stack_queue.bai_tap.reserve_elements_array_use_stack;
 
 
-public class Reserve {
+import java.util.Stack;
 
+public class Reserve {
+    public static void reverseInterger(int[] array) {
+        Stack< Integer > arrayNumber = new Stack();
+        for (int i = 0; i < array.length; i++) {
+            arrayNumber.push(array[i]);
+        }
+        for (int i = 0; i < array.length; i++) {
+            array[i] = arrayNumber.pop();
+        }
+    }
+    public static String reverseString(String string) {
+        Stack< Character > arrayString = new Stack();
+        for (int i = 0; i < string.length(); i++) {
+            arrayString.push(string.charAt(i));
+        }
+        String newString = "";
+        for (int i = 0; i < string.length(); i++) {
+            newString += arrayString.pop();
+        }
+        return newString;
+    }
 
 }
