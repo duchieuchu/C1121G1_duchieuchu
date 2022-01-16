73705586447a915597c65a4cccd91a7854399bbe
TechCare <you@example.com>
2022-01-16 09:29:02 +0700

    baitap

--- module2/src/ss8_clean_code_refactoring/bai_tap/TennisGame.java
+++ module2/src/ss8_clean_code_refactoring/bai_tap/TennisGame.java
@@ -12,9 +12,9 @@ public class TennisGame {
         if (player1Score == player2Score) {
             checkPlayer1score(player1Score);
         } else if (player1Score >= 4 || player2Score >= 4) {
-            lonHonHoacBang4(player1Score, player2Score);
+           checkPlayer1ScoreAndPlayer2ScoreGreaterThanOrEqualTo4 (player1Score, player2Score);
         } else {
-            nhoHon3(player1Score, player2Score);
+            checkPlayer1ScoreAndPlayer2ScoreLessThan3(player1Score, player2Score);
         }
         return scoreResult;
     }
@@ -40,7 +40,7 @@ public class TennisGame {
         }
     }
 
-    public static void lonHonHoacBang4(int player1Score, int player2Score) {
+    public static void checkPlayer1ScoreAndPlayer2ScoreGreaterThanOrEqualTo4(int player1Score, int player2Score) {
         String scoreResult = " ";
         int minusResult = player1Score - player2Score;
         if (minusResult == 1) scoreResult = "Advantage player1";
@@ -49,7 +49,7 @@ public class TennisGame {
         else scoreResult = "Win for player2";
     }
 
-    public static void nhoHon3(int player1Score, int player2Score) {
+    public static void checkPlayer1ScoreAndPlayer2ScoreLessThan3(int player1Score, int player2Score) {
         String scoreResult = " ";
         int tempScore = 0;
         for (int i = 1; i < 3; i++) {
