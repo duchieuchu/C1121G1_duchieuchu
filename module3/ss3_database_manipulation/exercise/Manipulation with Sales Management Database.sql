ab6c91d281071faa8ef81c3d5c432c5fef3b43f4
TechCare <you@example.com>
2022-02-26 12:37:00 +0700

    baitap

--- module3/ss3_database_manipulation/exercise/Manipulation with Sales Management Database.sql
+++ module3/ss3_database_manipulation/exercise/Manipulation with Sales Management Database.sql
@@ -34,7 +34,8 @@ foreign key (pID)references product(pID)
 insert into customer values
 (1,'Minh Quan',10),
 (2,'Ngoc Oanh',20),
-(3,'Hong Ha',50);
+(3,'Hong Ha',50),
+(4, 'Hong Hanh', 50);
 
 insert into `Order`values
 (1,1,'2006-03-21',null),
@@ -48,5 +49,34 @@ values(1,'May Giat',3),
 (4,'Quat',1),
 (5,'Bep Dien',2);
 
-insert into order_detail
-values(1,1,3),(1,3,7),(1,4,2),(2,1,1),(3,1,8),(2,5,4),(2,3,3);
+insert into order_detail values
+(1,1,3),
+(1,3,7),
+(1,4,2),
+(2,1,1),
+(3,1,8),
+(2,5,4),
+(2,3,3);
+
+-- Hiển thị các thông tin  gồm oID, oDate, oPrice của tất cả các hóa đơn trong bảng Order
+-- SELECT * FROM `order`; hien thi het du lieu cua order
+select oID,oDate,oTotalPrice from `order`;
+
+-- Hiển thị danh sách các khách hàng đã mua hàng, và danh sách sản phẩm được mua bởi các khách
+select customer.cId, customer.cName,product.pName 
+from customer
+join `order` on customer.cID = `order`.cID
+join order_detail on `order`.oId =order_detail.oId
+join product on order_detail.pID = product.pID;
+
+-- Hiển thị tên những khách hàng không mua bất kỳ một sản phẩm nào
+select customer.cName from customer 
+left join `order` on customer.cID = `order`.cID
+where `order`.oID is null;
+
+-- Hiển thị mã hóa đơn, ngày bán và giá tiền của từng hóa đơn 
+select `order`.oId, `order`.oDate, sum(product.pPrice*order_detail.od_QTY) as oTotalPrice
+from `order`
+join order_detail on order_detail.oId = `order`.oId
+join product on product.pId = order_detail.pId
+group by `order`.oId
