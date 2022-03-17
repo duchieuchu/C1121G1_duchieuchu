d8d60f3accf8bb41b279d7d85fb0ab7470e48b5a
TechCare <you@example.com>
2022-03-17 11:29:35 +0700

    baitap

--- module3/ss12_jdbc_and_crud/practice/user_manager1/src/main/java/com/codegym/controller/UserServlet.java
+++ module3/ss12_jdbc_and_crud/practice/user_manager1/src/main/java/com/codegym/controller/UserServlet.java
@@ -35,6 +35,8 @@ public class UserServlet extends HttpServlet {
                     showEditForm(request, response);
                     break;
                 case "delete":
+                    deleteUser(request,response);
+                    break;
                 default:
                     listUser(request, response);
                     break;
@@ -42,6 +44,16 @@ public class UserServlet extends HttpServlet {
         } catch (SQLException ex) {
             throw new ServletException(ex);
         }
+    }
+
+    private void deleteUser(HttpServletRequest request, HttpServletResponse response) throws SQLException, IOException, ServletException {
+        int id = Integer.parseInt(request.getParameter("id"));
+        userDAO.deleteUser(id);
+
+        List<User> listUser = userDAO.selectAllUsers();
+        request.setAttribute("listUser", listUser);
+        RequestDispatcher dispatcher = request.getRequestDispatcher("user/list.jsp");
+        dispatcher.forward(request, response);
     }
 
     private void showEditForm(HttpServletRequest request, HttpServletResponse response) throws SQLException, IOException, ServletException {
