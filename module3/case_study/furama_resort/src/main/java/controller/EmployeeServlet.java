3b59d41f5bb04ff6d971da280f20b0a0af2a4a4b
TechCare <you@example.com>
2022-03-22 23:09:10 +0700

    baitap

--- module3/case_study/furama_resort/src/main/java/controller/EmployeeServlet.java
+++ module3/case_study/furama_resort/src/main/java/controller/EmployeeServlet.java
@@ -10,30 +10,30 @@ import java.sql.SQLException;
 public class EmployeeServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        String action = request.getParameter("action");
-        if (action == null) {
-            action = "";
-        }
-        try {
-            switch (action) {
-                case "create":
-//                    showAddEmployee(request, response);
-                    break;
-                case "edit":
-//                    showEditEmployee(request, response);
-                    break;
-                case "delete":
-//                    deleteEmployee(request, response);
-                    break;
-                case "search":
-                    break;
-                default:
+//        String action = request.getParameter("action");
+//        if (action == null) {
+//            action = "";
+//        }
+//        try {
+//            switch (action) {
+//                case "create":
+////                    showAddEmployee(request, response);
+//                    break;
+//                case "edit":
+////                    showEditEmployee(request, response);
+//                    break;
+//                case "delete":
+////                    deleteEmployee(request, response);
+//                    break;
+//                case "search":
+//                    break;
+//                default:
 //                    listEmployee(request, response);
-                    break;
-            }
-        } catch (SQLException ex) {
-            throw new ServletException(ex);
-        }
+//                    break;
+//            }
+//        } catch (SQLException ex) {
+//            throw new ServletException(ex);
+//        }
     }
 
 
