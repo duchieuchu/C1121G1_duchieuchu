522bf0f0b270b92edc2616366ff509e02111fa18
TechCare <you@example.com>
2022-06-20 21:25:47 +0700

    baitap

--- module4/ss3_view_and_thymelief/practice/customer_management/src/main/java/com/example/repository/CustomerRepositoryImpl.java
+++ module4/ss3_view_and_thymelief/practice/customer_management/src/main/java/com/example/repository/CustomerRepositoryImpl.java
@@ -4,8 +4,8 @@ import com.example.model.Customer;
 import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
+    import java.util.HashMap;
+    import java.util.List;
 import java.util.Map;
 
 @Repository
--- module4again/ss3_views_and_thymeleaf/exercise/product-management-thymeleaf/src/main/java/com/codegym/controller/ProductController.java
+++ module4again/ss3_views_and_thymeleaf/exercise/product-management-thymeleaf/src/main/java/com/codegym/controller/ProductController.java
@@ -5,10 +5,7 @@ import com.codegym.service.IProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -63,4 +60,11 @@ public class ProductController {
         return "/view";
     }
 
+
+    @GetMapping("/search")
+    public String listSearch(Model model,@RequestParam String name) {
+        List<Product> productList = this.iProductService.findAllByName(name);
+        model.addAttribute("productList", productList);
+        return "/home";
+    }
 }
