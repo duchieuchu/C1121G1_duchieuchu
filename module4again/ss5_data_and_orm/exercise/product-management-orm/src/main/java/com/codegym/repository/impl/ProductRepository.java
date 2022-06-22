f90480f4059ef6e09b26afc3e1e480108896b745
TechCare <you@example.com>
2022-06-22 10:28:31 +0700

    baitap

--- module4again/ss5_data_and_orm/exercise/product-management-orm/src/main/java/com/codegym/repository/impl/ProductRepository.java
+++ module4again/ss5_data_and_orm/exercise/product-management-orm/src/main/java/com/codegym/repository/impl/ProductRepository.java
@@ -5,6 +5,7 @@ import com.codegym.repository.BaseRepository;
 import com.codegym.repository.IProductRepository;
 import org.springframework.stereotype.Repository;
 
+import javax.persistence.EntityTransaction;
 import javax.persistence.TypedQuery;
 import java.util.List;
 
@@ -19,21 +20,33 @@ public class ProductRepository implements IProductRepository {
 
     @Override
     public void save(Product product) {
-
+        EntityTransaction entityTransaction = BaseRepository.entityManager.getTransaction();
+        entityTransaction.begin();
+        BaseRepository.entityManager.persist(product);
+        entityTransaction.commit();
     }
 
     @Override
     public void delete(Product product) {
-
+        EntityTransaction entityTransaction = BaseRepository.entityManager.getTransaction();
+        entityTransaction.begin();
+        BaseRepository.entityManager.remove(product);
+        entityTransaction.commit();
     }
 
     @Override
     public void update(Integer id, Product product) {
-
+        EntityTransaction entityTransaction = BaseRepository.entityManager.getTransaction();
+        entityTransaction.begin();
+        BaseRepository.entityManager.merge(product);
+        entityTransaction.commit();
     }
 
     @Override
     public Product findById(Integer id) {
-        return null;
+        TypedQuery<Product> typedQuery =
+                BaseRepository.entityManager.createQuery("select s from product1 s where s.id=:idProduct", Product.class);
+        typedQuery.setParameter("idProduct", id);
+        return typedQuery.getSingleResult();
     }
 }
