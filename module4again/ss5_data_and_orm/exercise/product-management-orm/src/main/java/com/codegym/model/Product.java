a0830ea4f089feb03e1f1e12bb5b83e5027663ab
TechCare <you@example.com>
2022-06-22 09:57:19 +0700

    baitap

---
+++ module4again/ss5_data_and_orm/exercise/product-management-orm/src/main/java/com/codegym/model/Product.java
@@ -0,0 +1,68 @@
+package com.codegym.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity(name = "product1")
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id1;
+    private String name1;
+    private String info1;
+    private Integer quantity1;
+    private String made1;
+
+    public Product(Integer id1, String name1, String info1, Integer quantity1, String made1) {
+        this.id1 = id1;
+        this.name1 = name1;
+        this.info1 = info1;
+        this.quantity1 = quantity1;
+        this.made1 = made1;
+    }
+
+    public Product() {
+    }
+
+    public Integer getId1() {
+        return id1;
+    }
+
+    public void setId1(Integer id1) {
+        this.id1 = id1;
+    }
+
+    public String getName1() {
+        return name1;
+    }
+
+    public void setName1(String name1) {
+        this.name1 = name1;
+    }
+
+    public String getInfo1() {
+        return info1;
+    }
+
+    public void setInfo1(String info1) {
+        this.info1 = info1;
+    }
+
+    public Integer getQuantity1() {
+        return quantity1;
+    }
+
+    public void setQuantity1(Integer quantity1) {
+        this.quantity1 = quantity1;
+    }
+
+    public String getMade1() {
+        return made1;
+    }
+
+    public void setMade1(String made1) {
+        this.made1 = made1;
+    }
+}
