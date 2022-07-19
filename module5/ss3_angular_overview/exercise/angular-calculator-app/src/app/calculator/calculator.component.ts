94d733f0a6afe0d477c2e077f62b0caaa849ba52
TechCare <you@example.com>
2022-07-19 15:41:16 +0700

    up-git-cho-mn-xem

---
+++ module5/ss3_angular_overview/exercise/angular-calculator-app/src/app/calculator/calculator.component.ts
@@ -0,0 +1,52 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-calculator',
+  templateUrl: './calculator.component.html',
+  styleUrls: ['./calculator.component.css']
+})
+export class CalculatorComponent implements OnInit {
+  number1: number;
+  number2: number;
+  result: string;
+
+  addition(number1: number, number2: number): string {
+    return (Number(number1) + Number(number2)).toString();
+  }
+
+  subtraction(number1: number, number2: number): string {
+    return (Number(number1) - Number(number2)).toString();
+  }
+
+  multiplication(number1: number, number2: number): string {
+    return (Number(number1) * Number(number2)).toString();
+  }
+
+  division(number1: number, number2: number): string {
+    return (Number(number1) / Number(number2)).toString();
+  }
+
+  calculator(s: string, number1: number, number2: number) {
+    switch (s) {
+      case '+':
+        this.result = this.addition(number1, number2);
+        break;
+      case '-':
+        this.result = this.subtraction(number1, number2);
+        break;
+      case 'x':
+        this.result = this.multiplication(number1, number2);
+        break;
+      case '/':
+        this.result = this.division(number1, number2);
+        break;
+    }
+  }
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ module5/ss3_angular_overview/practice/show-info-pet/src/app/app.component.spec.ts
@@ -0,0 +1,31 @@
+import { TestBed, async } from '@angular/core/testing';
+import { AppComponent } from './app.component';
+
+describe('AppComponent', () => {
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [
+        AppComponent
+      ],
+    }).compileComponents();
+  }));
+
+  it('should create the app', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app).toBeTruthy();
+  });
+
+  it(`should have as title 'show-info-pet'`, () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app.title).toEqual('show-info-pet');
+  });
+
+  it('should render title', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement;
+    expect(compiled.querySelector('.content span').textContent).toContain('show-info-pet app is running!');
+  });
+});
