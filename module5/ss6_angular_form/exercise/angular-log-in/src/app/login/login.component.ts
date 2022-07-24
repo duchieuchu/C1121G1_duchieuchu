f4d416725e3696b32156c8477f52fefc42e39816
TechCare <you@example.com>
2022-07-24 15:45:49 +0700

    up-git-cho-mn-xem

--- module5/ss6_angular_form/exercise/angular-log-in/src/app/login/login.component.ts
+++ module5/ss6_angular_form/exercise/angular-log-in/src/app/login/login.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit, Output} from '@angular/core';
-import {FormControl, FormGroup, Validators} from '@angular/forms';
+import {AbstractControl, FormControl, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -11,9 +11,28 @@ export class LoginComponent implements OnInit {
 
   constructor() {
     this.loginFormReactive = new FormGroup({
-      email: new FormControl('', [Validators.required]),
-      password: new FormControl('', [Validators.required])
+      email: new FormControl('', [Validators.required, Validators.pattern('^[a-zA-Z0-9.!#$%&’*+/=?^' +
+        '_`{|}~-]+@[a-zA-Z0-9-]+(?:\\.[a-zA-Z0-9-]+)*$')]),
+      password: new FormControl('', [Validators.required, Validators.minLength(6)])
     });
+  }
+
+  checkEmail(email: AbstractControl) {
+    let value = email.value;
+
+    if (value.email !== '1@gmail.com') {
+      return {'inValidEmail': true};
+    }
+    return null;
+  }
+
+  checkPassword(password: AbstractControl) {
+    let value = password.value;
+
+    if (value.password !== '111') {
+      return {'inValidPassword': true};
+    }
+    return null;
   }
 
   ngOnInit(): void {
